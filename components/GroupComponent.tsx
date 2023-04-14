1c8853c2c732b134a201844ca60ba5d61ec87e5f
ssssshel <angelarteaga989@gmail.com>
2023-04-14 07:49:58 -0500

    provisional client side for groups menu

--- components/GroupComponent.tsx
+++ components/GroupComponent.tsx
@@ -7,18 +7,23 @@ interface IGroupComponent {
 
 export default function GroupComponent({ group, teams }: IGroupComponent) {
 
+  // Se ordenan a los equipos por puntos, seguido de diferencia de goles y finalmente goles a favor
   const sortedTeams = [...teams].sort((a, b) => {
     if (a.pts !== b.pts) {
       return b.pts - a.pts
     } else {
-      return b.gd - a.gd
+      if (a.gd !== b.gd) {
+        return b.gd - a.gd
+      } else {
+        return b.gf - a.gf
+      }
     }
   })
 
   return (
     <div className="flex flex-col gap-5">
       <p className="text-2xl font-bold">Grupo {group}</p>
-      <Link href={`/groups/${3}`}>
+      <Link href={`/groups/${group}`}>
         <div className="border rounded-md shadow-md cursor-pointer hover:shadow-2xl border-slate-400">
           <table align="center" cellPadding={5} className="w-full col-span-1 text-lg border-collapse text-primary" >
             <thead className="bg-slate-300" >
@@ -37,17 +42,17 @@ export default function GroupComponent({ group, teams }: IGroupComponent) {
             <tbody>
 
               {
-                teams.map(({ name_en, team_id }) => (
+                sortedTeams.map(({ name_en, team_id, mp, g, d, l, gf, ga, gd, pts }) => (
                   <tr key={team_id} className="text-center border-t border-b border-slate-400">
                     <td className="font-bold">{name_en}</td>
-                    <td className="font-bold">3</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>0</td>
-                    <td>3</td>
-                    <td>2</td>
-                    <td>1</td>
-                    <td className="font-bold">5</td>
+                    <td className="font-bold">{mp}</td>
+                    <td>{g}</td>
+                    <td>{d}</td>
+                    <td>{l}</td>
+                    <td>{gf}</td>
+                    <td>{ga}</td>
+                    <td>{gd}</td>
+                    <td className="font-bold">{pts}</td>
                   </tr>
 
                 ))
